36baef5dec067629d29ad33ee9af78fadde7ee82
uendel.couto <uendel.couto@gmail.com>
2021-04-17 09:58:37 -0300

    seleciona elementos e carrega dados dinamicamente

--- script.js
+++ script.js
@@ -39,14 +39,26 @@ const quizData = [
         d: '2002',
         correct: 'b',
     },
-]
+];
 
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit')
 
-let currentQuestion = 0
+let currentQuiz = 0;
 
-loadQuiz()
+loadQuiz();
 
 function loadQuiz() {
+    const currentQuizData = quizData[currentQuiz];
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
 
-    currentQuestion++
+    currentQuiz++;
 }
