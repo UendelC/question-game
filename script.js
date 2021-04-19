49fd2e291d594d14a1c07f8223e5bdb16209f752
uendel.couto <uendel.couto@gmail.com>
2021-04-19 12:07:44 -0300

    contagem de score e apresenta várias questoes

--- script.js
+++ script.js
@@ -47,8 +47,12 @@ const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
 const submitBtn = document.getElementById('submit')
+const answersEls = document.querySelectorAll(".answer");
 
 let currentQuiz = 0;
+let answer = undefined;
+let score = 0;
+
 
 loadQuiz();
 
@@ -59,6 +63,43 @@ function loadQuiz() {
     b_text.innerText = currentQuizData.b;
     c_text.innerText = currentQuizData.c;
     d_text.innerText = currentQuizData.d;
-
-    currentQuiz++;
 }
+
+function getSelected() {
+    let answer = undefined;
+
+    answersEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answersEls.forEach((answer) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected();
+
+    console.log(score);
+
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+        currentQuiz++;
+        if(currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            //todo show score
+            alert('You finished" Get Yourself a drink!');
+        }
+    }
+});
